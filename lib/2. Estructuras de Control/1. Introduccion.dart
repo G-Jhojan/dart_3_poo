e1c043dffd2773b3f8c816db2d61355df922fd99
Jhojan <jhojanrock98@gmail.com>
2023-10-13 16:32:15 -0500

    Estructuras de Control; operadores

---
+++ lib/2. Estructuras de Control/1. Introduccion.dart
@@ -0,0 +1,32 @@
+void main(List<String> args) {
+  String titulo ='';
+  String contenido ='';
+  String operadores = '';
+
+
+  titulo = 'Estructuras del Control de Flujo';
+  print(titulo);
+
+  contenido = '- Los if evaluan expresiones booleanas. (True o False)';
+  print(contenido);
+  contenido = '- Condición true: entre en el bloque if (entrada a la app)';
+  print(contenido);
+  contenido = '- Condición false: entre en el else: (Login / Registro)';
+  print(contenido);
+
+  titulo = 'Pueden usarse directamente los booleanos (true/flase) u operadores que devuelvan booleanos';
+  print(titulo);
+
+  operadores = '== (igualdad)';
+  print(operadores);
+  operadores = '!= (desigualdad/ negación/ diferencia)';
+  print(operadores);
+  operadores = '>= (mayor o igual )';
+  print(operadores);
+  operadores = '<= (menor o igual)';
+  print(operadores);
+  operadores = '> (mayor)';
+  print(operadores);
+  operadores = '< (menor)';
+  print(operadores);
+}
---
+++ lib/2. Estructuras de Control/2. operadores.dart
@@ -0,0 +1,31 @@
+void main(List<String> args) {
+  //int a = 10;
+  //int b = 20;
+  //int c = 30;
+
+  //print(a > b); // false
+  //print(a < b); // true
+  //print(a >= b); // false
+  //print(a <= b); // true
+  //print(a == b); // false
+  //print(a != c); // true
+
+  String nombre = 'Johaan Wolfgang Von Goethe';
+
+  //print(nombre == 'Johaan Wolfgang Von Goethe'); // true
+  //print(nombre == 'jhojan');
+  //print(nombre == 'jhojan');
+  //print(nombre != 'Jhojan');
+
+  nombre = 'Flutter';
+  print(nombre.length > 'Jhojan'.length);
+
+  print(true == true); // true
+  print(true == false); // false
+  print(true == false); // false
+  print(true == false); // false
+  print(true != false); // true
+  print(true != true); // false
+
+
+}

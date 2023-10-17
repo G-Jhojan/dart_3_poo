3de4ebc3e25d8aeb46e6cb12d1a9bab036e8ecfc
Jhojan <jhojanrock98@gmail.com>
2023-10-17 18:07:21 -0500

    ejercicio básico de if_elseif_else

--- lib/2. Estructuras de Control/3. if_elseif_else.dart
+++ lib/2. Estructuras de Control/3. if_elseif_else.dart
@@ -1,7 +1,14 @@
 void main(List<String> args) {
-  int edad = 25;
+  int edad = 9;
+  int age = 12;
 
-  if (edad < 5 ) {
-    print(edad == 5);
+  if (edad > age ) {
+    print('La edad $edad es mayor que age $age ');
+  }
+  else if (edad >= age){
+    print('El valor de edad $edad es igual o menor que age $age');
+  }
+  else {
+    print('El numero es menor que 12');
   }
 }
